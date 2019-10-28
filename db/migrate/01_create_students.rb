ffe18fd3b3783fc33d13e947a9f7b22a37f84db0
John Guest <jcguest90@gmail.com>
2019-10-28 05:28:16 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,10 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+
+  def create 
+    creat_table :students do |t|
+      t.string :name 
+    end
+  end
+  
+  
 end
---
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -0,0 +1,11 @@
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
+
+  def change
+    add_column :artists do |c|
+      c.integer :grade  
+      c.string :birthdate
+    end
+  end
+  
+  
+end
